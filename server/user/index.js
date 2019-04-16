c18a7a9697120458768cfc3d1596b4e8f3a9c2e9
zhangqiang <13608164790zq93>
2019-04-16 16:00:11 +0800

    test

--- server/user/index.js
+++ server/user/index.js
@@ -1,55 +1,65 @@
-const sqlOperation = require('../FMDB/sqlOperation')
+const sqlOperation = require('../FMDB/sqlOperation');
 
 //获取总人数
 const getAllUser = (callback) => {
   sqlOperation.selectAll('user', function (result) {
 
     if (typeof callback === 'function') {
-      callback(result.length)
+      callback(result.length);
     }
     // console.log('result',result.length);
-  })
-}
+  });
+};
 const login = (app) => {
   app.post('/login', function (req, res, next) {
 
     //ps 如果 不注明 不返回  这里会报404  错误码
-    console.log('222')
+    console.log('222');
     // let id = req.body.id
     let sql = 'SELECT * FROM user WHERE username="' + req.body.name + '"'
     sqlOperation.operationData(sql, function (data) {
-      let result = {}
+      let result = {};
 
       getAllUser(function (userNum) {
         if (data.length > 0) {
           //  说明有数据
           result = {
             isOk: false,
-            status: 1,//账号已存在
+            status: 1, //账号已存在
             message: '该账号已存在',
             userNum
-          }
+          };
         } else {
           result = {
-            status: 2,//账号不存在
+            status: 2, //账号不存在
             isOk: true,
             userNum
-          }
+          };
           let iSql = 'INSERT INTO user(username)  VALUES ' + '("' + req.body.name + '")'
           sqlOperation.insertData(iSql, function (isSuccess) {
             if (isSuccess) {
-              console.log('数据已插入成功')
+              console.log('数据已插入成功');
             }
-          })
+          });
         }
-        res.send(result)
-        next()
-      })
+        res.send(result);
+        next();
+      });
 
-    })
+    });
 
-  })
+  });
 
 }
 
-exports.login = login
+const saveChatRecord = (app) => {
+  app.post('/saveRecord',function (req,res,next) {
+    let sql = 'INSERT INTO chat_room(sendId,time,content)'
+    sqlOperation.insertData(sql,function () {
+      
+    });
+  })
+
+};
+
+exports.login = login;
